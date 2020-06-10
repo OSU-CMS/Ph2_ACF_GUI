6546dc2268bb77213e09ec5f48028436424687ab
Brandon Manley <brandonmanley10@gmail.com>
2020-06-10 02:25:51 -0400

    fix CMSminiDAQ recipe

--- pixelGui/runTest.sh
+++ pixelGui/runTest.sh
@@ -2,5 +2,8 @@
 
 cd /home/bmanley/Ph2_ACF/
 source setup.sh
-mkdir newTest && cd newTest/
-CMSIT_miniDAQ
+mkdir newTest 
+cp settings/RD53Files/CMSIT_RD53.txt newTest/
+cp settings/CMSIT.xml newTest/
+cd newTest/
+CMSITminiDAQ
