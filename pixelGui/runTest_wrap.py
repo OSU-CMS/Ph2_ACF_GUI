4015fa58312ad981c93beafd83feb0c2800b8eb7
Brandon Manley <brandonmanley10@gmail.com>
2020-06-10 02:45:46 -0400

    switch wrapper to main

--- pixelGui/runTest_wrap.py
+++ pixelGui/runTest_wrap.py
@@ -1,3 +1,9 @@
 import os
 
-os.system('source /home/bmanley/Ph2_ACF_GUI/pixelGui/runTest.sh')
+os.chdir('/home/bmanley/Ph2_ACF')
+os.system('source setup.sh')
+os.system('mkdir newTest')
+os.system('cp settings/RD53Files/CMSIT_RD53.txt newTest/')
+os.system('cp settings/CMSIT.xml newTest/')
+os.chdir('/home/bmanley/Ph2_ACF/newTest')
+os.system('CMSITminiDAQ')
