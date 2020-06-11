9cbe958f2b13d479b1f7e7979f40df76685bb0c0
Brandon Manley <brandonmanley10@gmail.com>
2020-06-10 20:51:59 -0400

    fix paths in test wrapper

--- pixelGui/runTest_wrap.py
+++ pixelGui/runTest_wrap.py
@@ -1,9 +1,11 @@
 import os
 
-os.chdir('/home/bmanley/Ph2_ACF')
-os.system('source setup.sh')
-os.system('mkdir newTest')
-os.system('cp settings/RD53Files/CMSIT_RD53.txt newTest/')
-os.system('cp settings/CMSIT.xml newTest/')
-os.chdir('/home/bmanley/Ph2_ACF/newTest')
+pixelDir = '/home/bmanley/Ph2_ACF/'
+
+os.system('source {0}setup.sh'.format(pixelDir))
+os.system('rm -r {0}newTest'.format(pixelDir))
+os.system('mkdir {0}newTest'.format(pixelDir))
+os.system('cp {0}settings/RD53Files/CMSIT_RD53.txt {0}newTest/'.format(pixelDir))
+os.system('cp {0}settings/CMSIT.xml {0}newTest/'.format(pixelDir))
+os.chdir('{0}newTest'.format(pixelDir))
 os.system('CMSITminiDAQ')
