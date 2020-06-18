9b256ac62317ef47ba218e3bd918772c02367348
brandon <brandonmanley10@gmail.com>
2020-06-17 23:03:03 -0400

    Cleaning

--- Gui/CMSminiDAQ_wrap.py
+++ Gui/CMSminiDAQ_wrap.py
@@ -6,4 +6,6 @@ os.system('rm -r {0}newTest'.format(pixelDir))
 os.system('mkdir {0}newTest'.format(pixelDir))
 os.system('cp {0}settings/RD53Files/CMSIT_RD53.txt {0}newTest/'.format(pixelDir))
 os.system('cp {0}settings/CMSIT.xml {0}newTest/'.format(pixelDir))
+
+os.chdir('{}newTest'.format(pixelDir))
 os.system('source {0}setup.sh && cd {0}newTest && CMSITminiDAQ'.format(pixelDir))
--- Gui/runTest.sh
+++
@@ -1,9 +0,0 @@
-#! /bin/bash 
-
-cd /home/bmanley/Ph2_ACF/
-source setup.sh
-mkdir newTest 
-cp settings/RD53Files/CMSIT_RD53.txt newTest/
-cp settings/CMSIT.xml newTest/
-cd newTest/
-CMSITminiDAQ
