c133477747c80d7f00ddbf396112989d8c3c0ed6
Matt Joyce <joyce.279@osu.edu>
2023-05-16 14:01:46 -0400

    updated README

--- run_Docker.sh
+++ run_Docker.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 SOCK=/tmp/.X11-unix; XAUTH=/tmp/.docker.xauth; xauth nlist $DISPLAY | sed -e 's/^..../ffff/' | xauth -f $XAUTH nmerge -; chmod 777 $XAUTH;
 
+#### This block is in development ##########
 output=$(python3 -m serial.tools.list_ports -q)
 IFS=$'\n' ports=($output)
 devices=""
-devicelist=""
+
 for port in "${ports[@]}"; do
     if [ ! -z "$port" ]; then
         devices+="--device=$port "
@@ -12,9 +13,21 @@ for port in "${ports[@]}"; do
     
 done
 devices=$(echo $devices | xargs)
-
-
+#############################################
+## More stuff in development #############
+mydevices=""
+#mydevicelist=("/dev/ttyUSBLV" "/dev/ttyUSBHV" "/dev/ttyUSBPeltier" "/dev/ttyACM0")
+mydevicelist=("/dev/ttyACM0" "/dev/ttyUSBHV")
+for mydevice in "${mydevicelist[@]}"; do
+    mydevices+="--device=$mydevice "
+    mydevices+=" "
+    echo "device loaded $mydevices"
+done
+mydevices=$(echo $mydevices | xargs)
+echo $mydevices | xargs
+############################################
 
 #docker run --rm -ti -v $PWD:$PWD -v ${PWD}/Ph2_ACF/test:/home/cmsTkUser/Ph2_ACF_GUI/Ph2_ACF/test -v ${PWD}/data:/home/cmsTkUser/Ph2_ACF_GUI/data $devices -w $PWD  -e DISPLAY=$DISPLAY -v $XSOCK:$XSOCK -v $XAUTH:$XAUTH   -e XAUTHORITY=$XAUTH --net host  local/testimage
 #docker run --rm -ti -v $PWD:$PWD -v ${PWD}/Ph2_ACF/test:/home/cmsTkUser/Ph2_ACF_GUI/Ph2_ACF/test -v ${PWD}/data:/home/cmsTkUser/Ph2_ACF_GUI/data --device=/dev/ttyUSBHV --device=/dev/ttyUSBLV --device=/dev/ttyUSBPeltier --device=/dev/ttyACM0 -w $PWD  -e DISPLAY=$DISPLAY -v $XSOCK:$XSOCK -v $XAUTH:$XAUTH   -e XAUTHORITY=$XAUTH --net host  local/testimage
 docker run --rm -ti -v $PWD:$PWD -v ${PWD}/Ph2_ACF/test:/home/cmsTkUser/Ph2_ACF_GUI/Ph2_ACF/test -v ${PWD}/data:/home/cmsTkUser/Ph2_ACF_GUI/data --device=/dev/ttyUSBHV --device=/dev/ttyUSBLV --device=/dev/ttyUSBPeltier --device=/dev/ttyACM0 -w $PWD  -e DISPLAY=$DISPLAY -v $XSOCK:$XSOCK -v $XAUTH:$XAUTH   -e XAUTHORITY=$XAUTH --net host  majoyce2/ph2_acf_gui:version4.13
+#docker run --rm -ti -v $PWD:$PWD -v ${PWD}/Ph2_ACF/test:/home/cmsTkUser/Ph2_ACF_GUI/Ph2_ACF/test -v ${PWD}/data:/home/cmsTkUser/Ph2_ACF_GUI/data $mydevices -w $PWD  -e DISPLAY=$DISPLAY -v $XSOCK:$XSOCK -v $XAUTH:$XAUTH   -e XAUTHORITY=$XAUTH --net host  majoyce2/ph2_acf_gui:version4.13
