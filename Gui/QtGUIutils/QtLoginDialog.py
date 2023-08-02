294b38dcae05bdfe2c94d99a54ae0f4fe3a7a1e3
ryansantos1174 <ryansantos1174@gmail.com>
2023-08-01 22:56:50 -0400

    Cleaning up QtLoginDialog

--- Gui/QtGUIutils/QtLoginDialog.py
+++ Gui/QtGUIutils/QtLoginDialog.py
@@ -1,120 +1,98 @@
-from PyQt5 import QtCore
-from PyQt5.QtCore import *
-from PyQt5.QtGui import QPixmap
+"""
+Use to open up Qt window to connect to GUI and database.
+
+createLogin()
+checkLogin()
+switchMode()
+"""
+from PyQt5.QtCore import Qt, pyqtSignal
+from PyQt5.QtGui import QFont
 from PyQt5.QtWidgets import (
-    QAbstractItemView,
     QApplication,
     QCheckBox,
     QComboBox,
-    QDateTimeEdit,
-    QDial,
     QDialog,
     QGridLayout,
     QGroupBox,
-    QHBoxLayout,
     QLabel,
     QLineEdit,
-    QProgressBar,
     QPushButton,
-    QRadioButton,
-    QScrollBar,
-    QSizePolicy,
-    QSlider,
-    QSpinBox,
-    QStyleFactory,
-    QTableView,
-    QTableWidget,
-    QTabWidget,
-    QTextEdit,
-    QHBoxLayout,
-    QVBoxLayout,
-    QWidget,
-    QMainWindow,
-    QMessageBox,
-    QSplitter,
 )
-
 import sys
-import os
-import subprocess
-from subprocess import Popen, PIPE
-
-from Gui.GUIutils.settings import *
-from Gui.GUIutils.DBConnection import *
+import Gui.GUIutils.settings as settings
 
 
 class QtLoginDialog(QDialog):
-    def __init__(self, master):
+    """Use to create login screen to open GUI and connect to database."""
+
+    login_signal = pyqtSignal(dict)
+
+    def __init__(self):
+        """Initialize class."""
         super().__init__()
-        self.master = master
         self.expertMode = False
-        self.connection = "Offline"
-        self.TryUsername = ""
-        self.TryPassword = ""
-        self.TryHostAddress = ""
-        self.TryDatabase = ""
 
         self.mainLayout = QGridLayout()
 
         self.createLogin()
         self.setLayout(self.mainLayout)
+        self.show()
 
     def createLogin(self):
-        LoginGroupBox = QGroupBox("")
-        LoginGroupBox.setCheckable(False)
+        """Use to create window for login displayed when starting the GUI."""
+        login_groupbox = QGroupBox("")
+        login_groupbox.setCheckable(False)
 
-        UsernameLabel = QLabel("Username:")
-        self.UsernameEdit = QLineEdit("")
-        self.UsernameEdit.setEchoMode(QLineEdit.Normal)
-        self.UsernameEdit.setPlaceholderText("Your username")
-        self.UsernameEdit.setMinimumWidth(220)
-        self.UsernameEdit.setMaximumWidth(260)
-        self.UsernameEdit.setMaximumHeight(30)
+        title_label = QLabel('<font size="12"> Phase2 Pixel Module Test </font>')
+        title_label.setFont(QFont("Courier"))
+        title_label.setMaximumHeight(30)
 
-        PasswordLabel = QLabel("Password:")
-        self.PasswordEdit = QLineEdit("")
-        self.PasswordEdit.setEchoMode(QLineEdit.Password)
-        self.PasswordEdit.setPlaceholderText("Your password")
-        self.PasswordEdit.setMinimumWidth(220)
-        self.PasswordEdit.setMaximumWidth(260)
-        self.PasswordEdit.setMaximumHeight(30)
+        username_label = QLabel("Username:")
+        self.username_edit = QLineEdit("")
+        self.username_edit.setEchoMode(QLineEdit.Normal)
+        self.username_edit.setPlaceholderText("Your username")
+        self.username_edit.setMinimumWidth(220)
+        self.username_edit.setMaximumWidth(260)
+        self.username_edit.setMaximumHeight(30)
 
-        HostLabel = QLabel("HostName:")
+        password_label = QLabel("Password:")
+        self.password_edit = QLineEdit("")
+        self.password_edit.setEchoMode(QLineEdit.Password)
+        self.password_edit.setPlaceholderText("Your password")
+        self.password_edit.setMinimumWidth(220)
+        self.password_edit.setMaximumWidth(260)
+        self.password_edit.setMaximumHeight(30)
 
-        if self.expertMode == False:
-            self.HostName = QComboBox()
-            self.HostName.addItems(dblist)
-            self.HostName.currentIndexChanged.connect(self.changeDBList)
-            HostLabel.setBuddy(self.HostName)
+        host_label = QLabel("HostName:")
+
+        host_edit = QLineEdit("128.146.38.1")
+        host_edit.setEchoMode(QLineEdit.Normal)
+        host_edit.setMinimumWidth(220)
+        host_edit.setMaximumWidth(260)
+        host_edit.setMaximumHeight(30)
+
+        database_edit = QLineEdit("SampleDB")
+        database_edit.setEchoMode(QLineEdit.Normal)
+        database_edit.setMinimumWidth(220)
+        database_edit.setMaximumWidth(260)
+        database_edit.setMaximumHeight(30)
+
+        self.disable_checkbox = QCheckBox("&Do not connect to DB")
+        self.disable_checkbox.setMaximumHeight(30)
+        if not self.expertMode:
+            self.host_name = QComboBox()
+            self.host_name.addItems(settings.dblist)
+            host_label.setBuddy(self.host_name)
+            self.database_combo = QComboBox()
+            self.DBNames = self.host_name.currentText() + ".All_list"
+            self.database_combo.addItems([self.DBNames])
+            self.database_combo.setCurrentIndex(0)
+            self.disable_checkbox.toggled.connect(host_edit.setDisabled)
+            self.disable_checkbox.toggled.connect(database_edit.setDisabled)
         else:
-            HostLabel.setText("HostIPAddr")
-            HostEdit = QLineEdit("128.146.38.1")
-            HostEdit.setEchoMode(QLineEdit.Normal)
-            HostEdit.setMinimumWidth(220)
-            HostEdit.setMaximumWidth(260)
-            HostEdit.setMaximumHeight(30)
-
-        DatabaseLabel = QLabel("Database:")
-        if self.expertMode == False:
-            self.DatabaseCombo = QComboBox()
-            self.DBNames = self.HostName.currentText() + ".All_list"
-            self.DatabaseCombo.addItems(self.DBNames)
-            self.DatabaseCombo.setCurrentIndex(0)
-        else:
-            self.DatabaseEdit = QLineEdit("SampleDB")
-            self.DatabaseEdit.setEchoMode(QLineEdit.Normal)
-            self.DatabaseEdit.setMinimumWidth(220)
-            self.DatabaseEdit.setMaximumWidth(260)
-            self.DatabaseEdit.setMaximumHeight(30)
-
-        self.disableCheckBox = QCheckBox("&Do not connect to DB")
-        self.disableCheckBox.setMaximumHeight(30)
-        if self.expertMode:
-            self.disableCheckBox.toggled.connect(self.HostEdit.setDisabled)
-            self.disableCheckBox.toggled.connect(self.DatabaseEdit.setDisabled)
-        else:
-            self.disableCheckBox.toggled.connect(self.HostName.setDisabled)
-            self.disableCheckBox.toggled.connect(self.DatabaseCombo.setDisabled)
+            host_label.setText("HostIPAddr")
+            self.disableCheckBox.toggled.connect(self.host_name.setDisabled)
+            self.disableCheckBox.toggled.connect(self.database_combo.setDisabled)
 
         button_login = QPushButton("&Login")
         button_login.setDefault(True)
@@ -122,76 +100,47 @@ class QtLoginDialog(QDialog):
 
         layout = QGridLayout()
         layout.setSpacing(20)
-        layout.addWidget(TitleLabel, 0, 1, 1, 3, Qt.AlignCenter)
-        layout.addWidget(UsernameLabel, 1, 1, 1, 1, Qt.AlignRight)
-        layout.addWidget(self.UsernameEdit, 1, 2, 1, 2)
-        layout.addWidget(PasswordLabel, 2, 1, 1, 1, Qt.AlignRight)
-        layout.addWidget(self.PasswordEdit, 2, 2, 1, 2)
-
-        if self.expertMode:
-            layout.addWidget(self.HostEdit, 2, 2, 1, 2)
-        else:
-            layout.addWidget(self.HostName, 2, 2, 1, 2)
-        layout.addWidget(DatabaseLabel, 3, 1, 1, 1, Qt.AlignRight)
-        if self.expertMode:
-            layout.addWidget(self.DatabaseEdit, 3, 2, 1, 2)
-        else:
-            layout.addWidget(self.DatabaseCombo, 3, 2, 1, 2)
-        layout.addWidget(self.expertCheckBox, 4, 2, 1, 1, Qt.AlignRight)
+        layout.addWidget(title_label, 0, 1, 1, 3, Qt.AlignCenter)
+        layout.addWidget(username_label, 1, 1, 1, 1, Qt.AlignRight)
+        layout.addWidget(self.username_edit, 1, 2, 1, 2)
+        layout.addWidget(password_label, 2, 1, 1, 1, Qt.AlignRight)
+        layout.addWidget(self.password_edit, 2, 2, 1, 2)
         layout.addWidget(button_login, 5, 1, 1, 3)
+
         layout.setRowMinimumHeight(6, 50)
 
         layout.setColumnStretch(0, 1)
         layout.setColumnStretch(1, 1)
         layout.setColumnStretch(2, 2)
         layout.setColumnStretch(3, 1)
-        LoginGroupBox.setLayout(layout)
-        self.mainLayout.addWidget(self.LoginGroupBox, 0, 0)
 
-    def destroyLogin(self):
-        self.LoginGroupBox.deleteLater()
-        self.mainLayout.removeWidget(self.LoginGroupBox)
+        login_groupbox.setLayout(layout)
+        self.logo_groupbox = QGroupBox("")
+
+        self.mainLayout.addWidget(login_groupbox, 0, 0)
 
     def checkLogin(self):
-        msg = QMessageBox()
-        expertList = ["mjoyce", "kwei"]  # temporary fix for demonstration
-        if self.UsernameEdit.text() in expertList:
+        """
+        Use to check credentials used in login dialog.
+
+        will emit signal that needs to be connected to determine
+        whether to launch simplied or main GUI
+        """
+        if self.username_edit.text() in settings.ExpertUserList:
             self.expertMode = True
-        if self.expertMode == True:
-            self.TryUsername = self.UsernameEdit.text()
-            self.TryPassword = self.PasswordEdit.text()
-            # self.TryHostAddress = DBServerIP[str(self.HostName.currentText())]
-            self.TryHostAddress = eval(self.HostName.currentText() + ".DBIP")
-            self.TryDatabase = str(self.DatabaseCombo.currentText())
-        #           self.TryHostAddress = self.HostEdit.text()
-        #           self.TryDatabase = self.DatabaseEdit.text()
-        else:
-            self.TryUsername = self.UsernameEdit.text()
-            self.TryPassword = self.PasswordEdit.text()
-            self.TryHostAddress = eval(self.HostName.currentText() + ".DBIP")
-            self.TryDatabase = str(self.DatabaseCombo.currentText())
 
-        self.connection = QtStartConnection(
-            self.TryUsername, self.TryPassword, self.TryHostAddress, self.TryDatabase
-        )
+        connection_parameters = {"username": self.username_edit.text(),
+                                 "password": self.password_edit.text(),
+                                 "address":
+                                 self.host_name.currentText() + ".DBIP",
+                                 "database": self.database_combo.currentText(),
+                                 "expert": self.expertMode}
 
-        if isActive(self.connection):
-            self.setConnection()
-        else:
-            QMessageBox().information(
-                None, "Error", "Database not connected", QMessageBox.Ok
-            )
+        self.login_signal.emit(connection_parameters)
 
-    def setConnection(self):
-        self.master.TryUsername = self.TryUsername
-        self.master.TryPassword = self.TryPassword
-        self.master.TryHostAddress = self.TryHostAddress
-        self.master.TryDatabase = self.TryDatabase
-        self.master.connection = self.connection
-        self.master.update()
-        self.accept()
-
+    # Currently doesn't work
     def switchMode(self):
+        """Use to switch between expert and simplified mode."""
         if self.expertMode:
             self.expertMode = False
         else:
@@ -199,8 +148,9 @@ class QtLoginDialog(QDialog):
         self.destroyLogin()
         self.createLogin()
 
-    def changeDBList(self):
-        self.DBNames = self.HostName.currentText() + ".DBName"
-        self.DatabaseCombo.clear()
-        self.DatabaseCombo.addItems(self.DBNames)
-        self.DatabaseCombo.setCurrentIndex(0)
+
+if __name__ == "__main__":
+    app = QApplication([])
+    login_window = QtLoginDialog()
+    login_window.login_signal.connect(lambda x: print(x.items()))
+    sys.exit(app.exec_())
