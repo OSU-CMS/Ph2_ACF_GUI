57aa7241438265ee688a31440a9e894ebabde97d
Ryan De Los Santos <ryansantos1174@gmail.com>
2023-08-22 13:05:04 -0400

    Defined FirmwareList

--- Gui/GUIutils/settings.py
+++ Gui/GUIutils/settings.py
@@ -11,6 +11,10 @@ ExpertUserList = [
     "localexpert",
 ]
 
+FirmwareList = {
+    "fc7.board.1": "192.168.1.80",
+    "fc7.board.2": "127.0.0.1",  #'192.168.1.81',
+}
 
 """
 if os.path.isfile(os.environ.get('GUI_dir')+"/fc7_ip_address.txt"):
