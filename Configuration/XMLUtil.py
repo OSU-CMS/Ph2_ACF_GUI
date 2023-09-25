e62ca2d23cdef62a3cae266953adf1bd2de59ab4
Matt Joyce <joyce.279@osu.edu>
2023-09-24 20:09:05 -0400

    bring back board,OG,module class

--- Configuration/XMLUtil.py
+++ Configuration/XMLUtil.py
@@ -59,6 +59,122 @@ class HWDescription():
     self.BeBoardList = []
     self.Settings = {}
 
+class Module():
+    def __init__(self,serialNo,moduleId,chipInfo,status="1"):
+        self.serialNo = serialNo
+        self.moduleId = moduleId #input for “FMC port:” in GUI
+        self.status = status # control by a click mark in gui start window.
+        self.Files = "./" # following the xml file we have now
+        self.moduleType = None #I need to add a method get module type automatically. #module type is RD53a?? or tfpx
+        self.chipType = None
+        self.test = None
+        self.chipInfo = chipInfo
+        self.chipList = []
+        self.setModuleType()
+        self.SetChipType()
+        self.adding_chips()
+        
+
+
+    def adding_chips(self):
+        IdLaneDict=ModuleLaneMap[self.moduleType]
+        numberOfChips = len(IdLaneDict)
+        print("self.chipInfo:" + str(self.chipInfo))
+        for chip in self.chipInfo:    
+            if not chip.getStatus():
+                continue
+            VDDA=chip.getVDDA()
+            VDDD=chip.getVDDD()
+            ChipId = chip.getID()
+            Lane = chip.getLane()
+            ConfigfileName = f"CMSIT_RD53_{self.serialNo}_{self.moduleId}_{ChipId}.txt"   #follow the guide in GenerateXMLConfig() in guituil.py
+            self.chipList.append([ChipId,Lane,ConfigfileName,VDDA,VDDD])
+      
+
+    def setModuleType(self):
+        if 'ZH' in self.serialNo:
+            self.moduleType = "TFPX Quad"
+        if 'SCC' in self.serialNo:
+            self.moduleType = "SingleSCC"
+        if 'RH' in self.serialNo:
+            self.moduleType = "CROC 1x2"
+        # there should be more module type
+    
+    def getModuleType(self):
+        return self.moduleType
+
+    def SetChipType(self):
+        if 'CROC' in self.moduleType:
+            self.chipType = "RD53B"
+        else:
+            self.chipType = "RD53A"
+
+    def getChipType(self):
+        return self.chipType
+        
+    
+    def get_Chip_info(self):
+        return self.chipList
+    
+    def getting_globalSetting(self):
+        if self.moduleType == "RD53A":
+            GO_Dict = globalSettings_DictA[self.test]
+        else:
+            GO_Dict = globalSettings_DictB[self.test]
+        return GO_Dict
+            
+    def setTest(self,testName):
+        self.test = testName
+        
+class OGModule():
+    def __init__(self,OGID,FMCID):
+        self.Id=OGID
+        self.FMCId=FMCID
+        self.isOpticalLink = False
+        #self.HyBridList = []
+        self.moduleList = []
+        self.test = None
+
+    def SetOpticalGrp(self, Id, FMCId, isOptLink=False):
+        self.Id=Id
+        self.FMCId=FMCId
+        self.isOpticalLink = isOptLink
+
+    def adding_module(self,NewModule,chipInfo):
+        NewModule.test =self.test
+        NewModule.chipInfo = chipInfo
+        self.moduleList.append(NewModule)
+
+    #def AddHyBrid(self, HybridModule):
+    #    self.HyBridList.append(HybridModule)
+
+class board():
+    def __init__(self,boardID,testName):
+        #self.moduleList = []
+        self.OGList = {}
+        #self.OpticalGroupDict = {'Id':"0",'FMCId':"0"}
+        self.boardID = boardID
+        self.boardType = "RD53"
+        self.eventType = "VR"
+        self.test = testName
+        self.ip_address = '0.0.0.0'
+        self.id = "cmsinnertracker.crate0.slot0" 
+        self.uri = "chtcp-2.0://localhost:10203?target={0}:50001".format(self.ip_address)
+        self.address_table = "file://${PH2ACF_BASE_DIR}/settings/address_tables/CMSIT_address_table.xml"
+
+    def addOG(self,OGID="0",FMCID="0"):
+        OG0=OGModule(OGID,FMCID)
+        OG0.test = self.test
+        #dict = {OGID:OG0}
+        self.OGList[OGID] = OG0
+
+    def GetTest(self): 
+        return self.test
+
+    def add_connection(self,address_table,connectionID,uri):
+        self.address_table = address_table
+        self.connectionID = connectionID
+        self.uri = uri
 
 class Device():
   def __init__(self):
