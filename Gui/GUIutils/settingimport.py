10b38ed9e4e3c7d9d4ba8ea97c538cb51e7444f2
David M Kim <davidkim918@localhost.localdomain>
2023-09-28 15:29:22 -0400

    changed to fetch parameters from siteSettings.py, not settings.py. Also combined two lists into a dictionary.

--- Gui/GUIutils/settingimport.py
+++ Gui/GUIutils/settingimport.py
@@ -1,24 +1,42 @@
-import settings as set
+from Ph2_ACF_GUI.Gui import siteSettings as set
 import csv
-
 
 class csvImport:
     def __init__(self):
-        ##The length of settingnamelist and settingslist should match
-        
-        self.settingnamelist = ['ExpertUserList','FirmwareList','dblist','FPGAConfigList','ModuleType','firmware_image','ModuleLaneMap',
-                'BoxSize','HVPowerSupplyModel','LVPowerSupplyModel','PowerSupplyModel_Termination','PowerSupplyModel_XML_Termination',
-                'ConfigFiles','Test','TestName2File','SingleTest','CompositeTest','CompositeList','pretuningList','tuningList',
-                'posttuningList','updatedXMLValues','updatedGlobalValue','stepWiseGlobalValue','header']
+        self.settingslist = {
+            'GPIB_DebugMode': set.GPIB_DebugMode,
+            'defaultACFVersion': set.defaultACFVersion,
+            'defaultFC7': set.defaultFC7,
+            'defaultFC7IP': set.defaultFC7IP,
+            'defaultFMC': set.defaultFMC,
+            'defaultUSBPortHV': set.defaultUSBPortHV,
+            'defaultHVModel': set.defaultHVModel,
+            'defaultUSBPortLV': set.defaultUSBPortLV,
+            'defaultLVModel': set.defaultLVModel,
+            'defaultModuleType': set.defaultModuleType,
+            'defaultPowerMode': set.defaultPowerMode,
+            'defaultHVCurrentCompliance': set.defaultHVCurrentCompliance,
+            'defaultHVsetting': set.defaultHVsetting,
+            'defaultSensorBaudRate': set.defaultSensorBaudRate,
+            'defaultDBServerIP': set.defaultDBServerIP,
+            'defaultDBName': set.defaultDBName,
+            'defaultTargetThr': set.defaultTargetThr,
+            'defaultSLDOscanVoltage': set.defaultSLDOscanVoltage,
+            'defaultSLDOscanMaxCurrent': set.defaultSLDOscanMaxCurrent,
+            'ModuleCurrentMap': set.ModuleCurrentMap,
+            'ModuleVoltageMapSLDO': set.ModuleVoltageMapSLDO,
+            'ModuleVoltageMap': set.ModuleVoltageMap,
+            'usePeltier': set.usePeltier,
+            'defaultPeltierPort': set.defaultPeltierPort,
+            'defaultPeltierBaud': set.defaultPeltierBaud,
+            'defaultPeltierSetTemp': set.defaultPeltierSetTemp,
+            'defaultPeltierMaxTemp': set.defaultPeltierMaxTemp,
+            'defaultPeltierMaxTempDiff': set.defaultPeltierMaxTempDiff
+        }
 
-        self.settingslist = [set.ExpertUserList,set.FirmwareList,set.dblist,set.FPGAConfigList,set.ModuleType,set.firmware_image,set.ModuleLaneMap,
-                set.BoxSize,set.HVPowerSupplyModel,set.LVPowerSupplyModel,set.PowerSupplyModel_Termination,set.PowerSupplyModel_XML_Termination,
-                set.ConfigFiles,set.Test,set.TestName2File,set.SingleTest,set.CompositeTest,set.CompositeList,set.pretuningList,set.tuningList,
-                set.posttuningList,set.updatedXMLValues,set.updatedGlobalValue,set.stepWiseGlobalValue,set.header]
+        self.updated_settingslist = {}
 
-        self.updated_settingslist = []
-
-    def read_csv(self,readpath):
+    def read_csv(self, readpath):
         updated_settingslist = self.settingslist.copy()  # Create a copy to preserve defaults
 
         try:
@@ -28,13 +46,11 @@ class csvImport:
 
                 for row in reader:  # Iterate through the rows of the CSV file
                     for header in headers_in_csv:
-                        if header in self.settingnamelist:
-                            index = self.settingnamelist.index(header)
-                            updated_settingslist[index] = row[header]
+                        if header in self.settingslist:
+                            updated_settingslist[header] = row[header]
                         else:
-                            print(f'The settingnamelist does not contain the header "{header}".\n' 
-                                f'Either update the settingnamelist or check if "{header}" has any typos.')
-                            raise KeyError()
+                            print(f'The settingslist does not contain the header "{header}".\n'
+                                  f'Either update the settingslist or check if "{header}" has any typos.')
 
             print(f"CSV file '{readpath}' read successfully.")
             return updated_settingslist
@@ -42,27 +58,24 @@ class csvImport:
         except Exception as e:
             print(f"Error reading CSV file: {str(e)}")
 
-    def create_csv(self,writepath):
-        createddictionary = dict(zip(self.settingnamelist, self.settingslist))
-
+    def create_csv(self, writepath):
         try:
             # Open the CSV file for writing
             with open(writepath, 'w', newline='') as csv_file:
                 writer = csv.writer(csv_file)
-                
+
                 # Write the headers (keys of the dictionary)
-                headers = createddictionary.keys()
+                headers = self.settingslist.keys()
                 writer.writerow(headers)
-                
+
                 # Write the corresponding values (associated with each header)
-                values = [createddictionary[header] for header in headers]
+                values = [self.settingslist[header] for header in headers]
                 writer.writerow(values)
 
             print(f"CSV file '{writepath}' created successfully.")
 
         except Exception as e:
             print(f"Error creating CSV file: {str(e)}")
-    
 
 if __name__ == "__main__":
     test = csvImport()
