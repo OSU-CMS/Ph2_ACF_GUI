d59da23f832a0a032f7d1bd7cb8a882a3c0e23a3
ryansantos1174 <ryansantos1174@gmail.com>
2023-10-10 19:49:50 -0400

    Added signals to SLDO Curve

--- Gui/python/SLDOScanHandler.py
+++ Gui/python/SLDOScanHandler.py
@@ -2,14 +2,15 @@
 Class to perform the SLDO curve scanning
 """
 
-from PyQt5.QtCore import QObject, QThread, pyqtSignal
+from PyQt5.QtCore import QThread, pyqtSignal
 from Gui.python.logging_config import logger
 import numpy as np
-import numpy.typing as npt
 
 
-class sldo_curve_worker(QObject):
+class SLDOCurveWorker(QThread):
     finished = pyqtSignal()
+    progress = pyqtSignal()
+    measure = pyqtSignal(np.array)
 
     def __init__(
         self,
@@ -29,11 +30,15 @@ class sldo_curve_worker(QObject):
         self.integration_cycles = integration_cycles
         self.starting_current = starting_current
         self.pin_list = pin_list
+        self.exiting = False
 
-    def run(self) -> npt.NDArray[np.float_]:
+    def run(self) -> None:
         """
         Run thread that will ramp up the LV while measuring from the multimeter, then ramp down doing the same thing.
         Combine the two list of measurments and return that from the function.
+
+        measure: emitted after VI curve of each pin contains array of MM measurements
+        progress: emitted after each cycle of the LV sweep
         """
         # Turn off instruments
         self.instrument_cluster.off()
@@ -51,7 +56,10 @@ class sldo_curve_worker(QObject):
                 measure_function=self.instrument_cluster.multimeter_measure,
                 measure_args={"cycles": self.integration_cycles},
                 log_function=logger.info,
+                execute_each_step=self.progress.emit,
+                break_monitoring=
             )
+
             result_up = np.array(result)
             # Ramp down LV and measure on the way down
             _, result = self.instrument_cluster._lv.sweep(
@@ -64,11 +72,44 @@ class sldo_curve_worker(QObject):
                 measure_function=self.instrument_cluster.multimeter_measure,
                 measure_args={"cycles": self.integration_cycles},
                 log_function=logger.info,
+                execute_each_step=self.progress.emit,
+                break_monitoring= self.breakTest
             )
             result_down = np.array(result)
             total_result = np.concatenate((result_up, result_down), axis=0)
-
             self.instrument_cluster.lv_off(None)
+            self.measure.emit(total_result)
 
-            # NOTE this should probably be emitted as a signal
-            return total_result
+    def breakTest(self):
+        """ Use to kill instrument sweep. """
+        if self.exiting:
+            return True
+        return False
+
+    def abortTest(self):
+        self.exiting = True
+
+class SLDOCurveHandler:
+    def __init__(self, instrument_cluster):
+        self.instrument_cluster = instrument_cluster
+        self.test = SLDOCurveHandler(instrument_cluster=self.instrument_cluster)
+        self.test.measure.connect(self.updateMeasurement)
+
+    def updateMeasurement(self):
+        pass
+
+    def SLDOCurve(self):
+        if not self.instrument_cluster:
+            return
+        self.test.start()
+
+    def finish(self):
+        self.instrument_cluster.off()
+
+    def stop(self):
+        try:
+            self.test.abortTest()
+            self.instrument_cluster.off()
+            self.test.terminate()
+        except Exception as err:
+            logger.error(f"Failed to stop the SLDO test due to error {err}")
