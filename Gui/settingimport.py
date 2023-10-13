4a6ea147c381be279e63eb367243f3adddeb5180
David M Kim <davidkim918@vpn1-users2-68.asc.ohio-state.edu>
2023-10-13 15:19:09 -0400

    added docstrings and comments, a few edits to the code for error, and moved the file so settingimport.py will be within the same directory as the siteSettings_template.py and siteSettings.py

--- Gui/settingimport.py
+++ Gui/settingimport.py
@@ -5,6 +5,11 @@ import logging
 logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 logger = logging.getLogger(__name__)
 
+'''
+The class csvImport parses variables from the default settings template and update their settings with values found from the csv file without changing the template.
+The updated settings are created as 'siteSettings.py' 
+Is also capable of creating a csv file from python file.
+'''
 
 class csvImport:
     def __init__(self):
@@ -12,34 +17,39 @@ class csvImport:
         self.updatedsiteSettingsPath = 'Gui/siteSettings.py'
         self.updated_settingslist = {}
         self.parser = variableParser()
+		#Parses using the variableParser class
         self.defaultSettings = self.parser.parse(self.settingsTemplatePath)
 
     def import_settings(self, csvpath):
         csvstringdata = self._read_csv(csvpath)
-        csvdata = self.parser.restoreOriginalType(csvstringdata)
+        csvdata = self.parser.restoreOriginalType(csvstringdata)	#Revert back to original data type
         self._update_settings(csvdata)
 
     def _read_csv(self, readpath):
         updated_settings = self.defaultSettings.copy()  # Create a copy to preserve defaults
         
-        noDefaultList = []
-        unUsedData = []
-        MissingDatalist = []
-                
+        noDefaultList = []      #List of settings from csv that template does not have. 
+                                #Just for information, the method will still add the settings to the output.
+        unUsedData = []         #List of data from csv that were not imported.
+        MissingDatalist = []    #List of settings that were skipped.
+
+        #The method will try to create settings from csv file, skipping problematic lines.
+
+
         try:
             with open(readpath, 'r', newline='') as csv_file:
                 reader = csv.reader(csv_file)
 
                 for row in reader:
-                    if len(row) == 2:
+                    if len(row) == 2:                               #Checks if each row has two columns, if not, skip.
                         header = row[0]
                         data = row[1]
 
-                        if header == '':
+                        if header == '':                            #Checks if the header is empty, if yes, skip.
                             unUsedData.append(data)
-                        elif header not in self.defaultSettings:
+                        elif header not in self.defaultSettings:    #Checks if the header is not defined in template. (will not skip, will import from csv)
                             noDefaultList.append(header)
-                            if data == '' or data == 'None':
+                            if data == '' or data == 'None':        #Checks if the data is None/empty, if yes, assign None.
                                 updated_settings[header] = None
                             else:
                                 updated_settings[header] = data
@@ -60,6 +70,7 @@ class csvImport:
                 logger.info(f'The following data {unUsedData} were not used because there was no header in the CSV file.')
             if len(MissingDatalist) != 0:
                 logger.info(f'The following list {MissingDatalist} were skipped because there was no value associated or csv file had more than two columns.')
+                logger.info('Check if the data is a dictionary/list, and if it is enclosed with only one pair of DOUBLE quotation marks.')
             
             return updated_settings
 
@@ -78,6 +89,7 @@ class csvImport:
         except Exception as e:
             logger.error(f"Error updating/creating siteSettings.py file: {str(e)}")
 
+    #Creates the csv file from a specific path or from the default template if not specified.
     def create_csv(self, writepath):
         try:
             # Open the CSV file for writing
@@ -97,5 +109,5 @@ class csvImport:
 
 if __name__ == "__main__":
     test = csvImport()
-    #create = test.create_csv('testcsv.csv')    #this will create the csv using the existing template
+    create = test.create_csv('testcsv.csv')    #this will create the csv using the existing template
     test.import_settings('testcsv.csv')
--- parseVariables.py
+++ parseVariables.py
@@ -1,13 +1,18 @@
 import ast
 
+'''
+The class variableParser parses variables from a python file, specifically settings and returns a dictionary.
+Is capable of restoring data types of variables inside any input dictionary. 
+'''
+
 class variableParser:
     def parse(self, file_path):
         variable_dict = {}
         
         with open(file_path, 'r') as module_file:
-            module_code = module_file.read()
+            module_code = module_file.read()        #reads in the file
 
-        module_ast = ast.parse(module_code)
+        module_ast = ast.parse(module_code)         #parses instances
 
         for node in ast.walk(module_ast):
             if isinstance(node, ast.Assign):
@@ -26,7 +31,7 @@ class variableParser:
             return None
 
     @staticmethod
-    def restoreOriginalType(input_dict):
+    def restoreOriginalType(input_dict):        #restores the datatype of a given dictionary
         converted_dict = {}
         for key, value in input_dict.items():
             converted_value = value
