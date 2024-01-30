10b930e7bb3bd8198aada2b5e4f54483114aa486
Matt Joyce <joyce.279@osu.edu>
2024-01-30 09:36:39 -0500

    removed block comment and fixed typo

--- Gui/siteConfig.py
+++ Gui/siteConfig.py
@@ -62,7 +62,7 @@ icicle_instrument_setup = { "lv":"KeysightE3633A",
                             "default_hv_voltage": -80,
                             "default_hv_compliance_current": 5e-6,
                             "default_hv_delay": 2,
-                            "default_hv_step_size": 10}
+                            "default_hv_step_size": 10,
                             "relay_board": "RelayBoard",
                             "relay_board_resource": "ASRL/dev/ttyUSB4::INSTR",
                             "multimeter": "HP34401A",
