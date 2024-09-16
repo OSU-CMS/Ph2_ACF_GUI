ebdec1907c8986c02cb7918e248d20b7988c7647
Santeri Saariokari <34010391+saarioka@users.noreply.github.com>
2024-09-16 19:18:39 +0300

    indent

--- Gui/GUIutils/guiUtils.py
+++ Gui/GUIutils/guiUtils.py
@@ -405,9 +405,9 @@ def GenerateXMLConfig(firmwareList, testName, outputDir, **arg):
                 moduleType = module.getModuleType()
                 
                 RxPolarities = None
-		        if "CROC" in moduleType:
+                if "CROC" in moduleType:
                     RxPolarities = "0"
-		        if "CROC" in moduleType and "Quad" in moduleType:
+                if "CROC" in moduleType and "Quad" in moduleType:
                     RxPolarities = "1" 
                 
                 revPolarity = not ("CROC" in moduleType and "1x2" in moduleType)
