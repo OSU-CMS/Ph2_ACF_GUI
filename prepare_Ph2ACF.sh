62c5dc79715c4deef7bb9109a2aaba5f3eafb270
mjoyce <joyce.279@osu.edu>
2024-12-11 13:59:09 -0500

    added option to use old image, fixed lane polarities

--- prepare_Ph2ACF.sh
+++ prepare_Ph2ACF.sh
@@ -6,6 +6,10 @@ source setup.sh
 cd ${GUI_dir}
 source symlinks.sh
 
+cd ${GUI_dir}/Gui/python
+
+wget -O rhapi.py https://gitlab.cern.ch/cms-ph2-database/resthub-dev/-/raw/master/clients/python/src/main/python/rhapi.py
+
 cd ${GUI_dir}/Gui
 
 echo "You can now open the GUI by doing 'python3 QtApplication.py'."
