76cdc79d61ddd19406fe376723214ba8668f5072
mjoyce <joyce.279@osu.edu>
2025-01-17 10:56:22 -0500

    merging several branches into one update

--- Gui/python/TestHandler.py
+++ Gui/python/TestHandler.py
@@ -727,8 +727,14 @@ class TestHandler(QObject):
                         os.environ.get("PH2ACF_BASE_DIR"), self.output_dir
                     )
                 )
-                # os.system("cp {0}/test/Results/Run000000*.txt {1}/".format(os.environ.get("PH2ACF_BASE_DIR"),self.output_dir))
-                # os.system("cp {0}/test/Results/Run000000*.xml {1}/".format(os.environ.get("PH2ACF_BASE_DIR"),self.output_dir))
+            elif "IVCurve" in self.currentTest:
+                os.system(
+                    "cp {0}/test/Results/Run{1}_MonitorDQM.root {2}/".format(
+                        os.environ.get("PH2ACF_BASE_DIR"),
+                        self.RunNumber,
+                        self.output_dir,
+                    )
+                )
             else:
                 os.system(
                     "cp {0}/test/Results/Run{1}*.root {2}/".format(
@@ -968,6 +974,7 @@ class TestHandler(QObject):
                 print('process would not terminate, so killing it now...')
                 self.run_process.kill()
         if "IVCurve" in self.currentTest:
+            self.saveTest()
             return
         # To be removed
         # if isCompositeTest(self.info):
@@ -1279,7 +1286,11 @@ class TestHandler(QObject):
     def upload_to_Panthera(self):
         try:
             self.runwindow.UploadButton.setDisabled(True)
+            counter = 0
+            nummodules = len(self.modules)
             for module in self.modules:
+                self.runwindow.ProgressBarLabel.setText("Uploading modules: ["+"##"*int(counter)+"=="*int(nummodules-counter)+"] "+str(counter)+"/"+str(nummodules))
+                QApplication.processEvents() #not ideal. May need to fix later.
                 status, message = self.felis.upload_results(
                     module.getModuleName(),
                     self.master.username,
@@ -1289,14 +1300,20 @@ class TestHandler(QObject):
                 )
                 if not status:
                     raise ConnectionError(message)
+                counter+=1
+                self.runwindow.ProgressBarLabel.setText("Upload successful!")
+
         except Exception as e:
             if not self.master.panthera_connected:
-                logger.error("Cannot upload test results, you are not signed in to Panthera.")
+                error_message = "Cannot upload test results, you are not signed in to Panthera."
+                logger.error(error_message)
             else:
-                logger.error(f"There was an error uploading the test results. {repr(e)}")
+                error_message = "There was an error uploading the test results."
+                logger.error(f"{error_message} {repr(e)}")
                 if self.autoSave:
                     self.runwindow.UploadButton.setDisabled(False) #if autosave fails, allow manual
-    
+            self.runwindow.ProgressBarLabel.setText(error_message)
+            
     def bumpbond_analysis(self):
         
         runNumber = "000000" if self.RunNumber == "-1" else self.RunNumber
