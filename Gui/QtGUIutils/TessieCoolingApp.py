7a3e36e608a4c3098ffe88865b5ce8eb7499185e
Ryan De Los Santos <ryansantos1174@gmail.com>
2025-03-18 16:39:41 -0400

    Cleanup of variable names and addition of timer to get values

--- Gui/QtGUIutils/TessieCoolingApp.py
+++ Gui/QtGUIutils/TessieCoolingApp.py
@@ -1,12 +1,18 @@
 from PyQt5 import QtWidgets
 from PyQt5.QtWidgets import QWidget
-from PyQt5.QtCore import Qt
+from PyQt5.QtCore import Qt, QTimer
+
 
 class Tessie(QWidget):
     def __init__(self, dimension):
         super(Tessie, self).__init__()
+
+        self.timer = QTimer()
+        self.timer.timeout.connect(self.update_values)
+        
         self.setupUi()
         self.show()
+
 
     def setupUi(self):
 
@@ -26,49 +32,49 @@ class Tessie(QWidget):
 
         self.setLayout(self.gridLayout)  # Set layout for the main window
 
-        self.TessieLabel = QtWidgets.QLabel("TESSIE (2024/06/19-06)")
-        self.CANbus_errors_label = QtWidgets.QLabel("CANbus errors")
+        self.tessie_label = QtWidgets.QLabel("TESSIE (2024/06/19-06)")
+        self.canbus_errors_label = QtWidgets.QLabel("CANbus errors")
         self.I2C_errors_label = QtWidgets.QLabel("I2C errors")
-        self.runtimeLabel = QtWidgets.QLabel("Runtime")
-        self.statusLabel = QtWidgets.QLabel("Status")
+        self.runtime_label = QtWidgets.QLabel("Runtime")
+        self.status_label = QtWidgets.QLabel("Status")
 
-        self.upperGridLayout.addWidget(self.TessieLabel, 0,0,1,2)
-        self.upperGridLayout.addWidget(self.CANbus_errors_label, 1,0,1,1)
+        self.upperGridLayout.addWidget(self.tessie_label, 0,0,1,2)
+        self.upperGridLayout.addWidget(self.canbus_errors_label, 1,0,1,1)
         self.upperGridLayout.addWidget(self.I2C_errors_label, 2,0,1,1)
-        self.upperGridLayout.addWidget(self.runtimeLabel, 3,0,1,1)
-        self.upperGridLayout.addWidget(self.statusLabel, 4,0,1,1)
+        self.upperGridLayout.addWidget(self.runtime_label, 3,0,1,1)
+        self.upperGridLayout.addWidget(self.status_label, 4,0,1,1)
 
-        self.CANbus_errors_edit = QtWidgets.QLabel("0")
-        self.I2C_errors_edit = QtWidgets.QLabel("0")
-        self.runtimeEdit = QtWidgets.QLabel("10346")
-        self.statusEdit = QtWidgets.QLabel("no problem")
+        self.canbus_errors_value = QtWidgets.QLabel("0")
+        self.I2C_errors_value = QtWidgets.QLabel("0")
+        self.runtime_value = QtWidgets.QLabel("10346")
+        self.status_value = QtWidgets.QLabel("no problem")
 
-        self.upperGridLayout.addWidget(self.CANbus_errors_edit, 1,1,1,1)
-        self.upperGridLayout.addWidget(self.I2C_errors_edit, 2,1,1,1)
-        self.upperGridLayout.addWidget(self.runtimeEdit, 3,1,1,1)
-        self.upperGridLayout.addWidget(self.statusEdit, 4,1,1,1)
+        self.upperGridLayout.addWidget(self.canbus_errors_value, 1,1,1,1)
+        self.upperGridLayout.addWidget(self.I2C_errors_value, 2,1,1,1)
+        self.upperGridLayout.addWidget(self.runtime_value, 3,1,1,1)
+        self.upperGridLayout.addWidget(self.status_value, 4,1,1,1)
         self.upperGridLayout.setColumnMinimumWidth(1, 75)
 
 
-        self.airLabel = QtWidgets.QLabel("Air [° C]")
-        self.waterLabel = QtWidgets.QLabel("Water [° C]")
+        self.air_label = QtWidgets.QLabel("Air [° C]")
+        self.water_label = QtWidgets.QLabel("Water [° C]")
         self.lid_status_label = QtWidgets.QLabel("Lid Status")
 
-        self.upperGridLayout.addWidget(self.airLabel, 1,2,1,1)
-        self.upperGridLayout.addWidget(self.waterLabel, 2,2,1,1)
+        self.upperGridLayout.addWidget(self.air_label, 1,2,1,1)
+        self.upperGridLayout.addWidget(self.water_label, 2,2,1,1)
         self.upperGridLayout.addWidget(self.lid_status_label, 3,2,1,1)
 
-        self.airEdit = QtWidgets.QLabel("23.25")
-        self.waterEdit = QtWidgets.QLabel("21.34")
-        self.lid_status_edit = QtWidgets.QLabel("locked")
+        self.air_value = QtWidgets.QLabel("23.25")
+        self.water_value = QtWidgets.QLabel("21.34")
+        self.lid_status_value = QtWidgets.QLabel("locked")
 
-        self.airEdit.setStyleSheet("QLabel { background-color: green; color: black; }")
-        self.waterEdit.setStyleSheet("QLabel { background-color: green; color: black; }")
-        self.lid_status_edit.setStyleSheet("QLabel { background-color: green; color: black; }")
+        self.air_value.setStyleSheet("QLabel { background-color: green; color: black; }")
+        self.water_value.setStyleSheet("QLabel { background-color: green; color: black; }")
+        self.lid_status_value.setStyleSheet("QLabel { background-color: green; color: black; }")
 
-        self.upperGridLayout.addWidget(self.airEdit, 1,3,1,1)
-        self.upperGridLayout.addWidget(self.waterEdit, 2,3,1,1)
-        self.upperGridLayout.addWidget(self.lid_status_edit, 3,3,1,1)
+        self.upperGridLayout.addWidget(self.air_value, 1,3,1,1)
+        self.upperGridLayout.addWidget(self.water_value, 2,3,1,1)
+        self.upperGridLayout.addWidget(self.lid_status_value, 3,3,1,1)
         self.upperGridLayout.setColumnMinimumWidth(3, 50)
 
         self.ref_hum_label = QtWidgets.QLabel("Ref. Hum.")
@@ -77,14 +83,14 @@ class Tessie(QWidget):
         self.upperGridLayout.addWidget(self.ref_hum_label, 1,4,1,1)
         self.upperGridLayout.addWidget(self.dew_point_label, 2,4,1,1)
 
-        self.ref_hum_edit = QtWidgets.QLabel("51.92")
-        self.dew_point_edit = QtWidgets.QLabel("12.83")
+        self.ref_hum_value = QtWidgets.QLabel("51.92")
+        self.dew_point_value = QtWidgets.QLabel("12.83")
 
-        self.ref_hum_edit.setStyleSheet("QLabel { background-color: yellow; color: black; }")
-        self.dew_point_edit.setStyleSheet("QLabel { background-color: green; color: black; }")
+        self.ref_hum_value.setStyleSheet("QLabel { background-color: yellow; color: black; }")
+        self.dew_point_value.setStyleSheet("QLabel { background-color: green; color: black; }")
 
-        self.upperGridLayout.addWidget(self.ref_hum_edit, 1,5,1,1)
-        self.upperGridLayout.addWidget(self.dew_point_edit, 2,5,1,1)
+        self.upperGridLayout.addWidget(self.ref_hum_value, 1,5,1,1)
+        self.upperGridLayout.addWidget(self.dew_point_value, 2,5,1,1)
         self.upperGridLayout.setColumnMinimumWidth(5, 50)
 
         self.stop_all_button = QtWidgets.QPushButton("STOP ALL")
@@ -108,6 +114,30 @@ class Tessie(QWidget):
             self.lowerGridLayout.addWidget(self.TEC_edits[i], 1, 2*i+2)
 
         self.setLayout(self.gridLayout)
+
+    def udpate_values():
+        var = coldbox.get_monitor_data("VAR", timeout)
+
+        can_errors = var["can_errors"]
+        i2c_errors = var["i2c_errors"]
+        runtime = var["runtime"]
+        status = var["valve0_status"]
+
+        air_temp = coldbox.get_air_temperature()
+        water_temp = coldbox.get_water_temperature()
+        rel_humidity = coldbox.get_relative_humidity()
+        dew_point = coldbox.get_dewpoint()
+        lid_status = coldbox.get_lid_status() 
+        tec_temperatures = coldbox.get_tec_temperature(channel="all")
+
+
+
+
+
+
+
+
+        
 
 
 if __name__ == "__main__":
