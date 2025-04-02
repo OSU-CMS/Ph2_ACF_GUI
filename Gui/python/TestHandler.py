d9d55afbeaad9012d148e76e5bdab8d161c90e08
mjoyce <joyce.279@osu.edu>
2025-04-02 16:18:54 -0400

    Added execute_each_step to more hv_ functions

--- Gui/python/TestHandler.py
+++ Gui/python/TestHandler.py
@@ -476,12 +476,21 @@ class TestHandler(QObject):
 
                 if testName == "SCurveScan_2100_FWD":
                     if hv_on_module:
-                        self.instruments.hv_off()
+                        starting_voltages = [
+                            np.abs(getattr(module["hv"], "voltage"))
+                            for module in self.instruments._module_dict.values()
+                        ]
+                        self.instruments.hv_off(
+                            execute_each_step=lambda: self.execute_each_step(starting_voltages)
+                        )
                         self.instruments.hv_on_module(
                             module=mod_dict[number],
                             voltage=site_settings.forward_bias_voltage,
                             delay=0.3,
                             step_size=10,
+                            execute_each_step=lambda: self.ramp_progress_bar(
+                            [site_settings.forward_bias_voltage] * len(self.instruments._module_dict.values())
+                            )
                         )
                     else:
                         self.instruments.hv_on_module(
@@ -489,6 +498,9 @@ class TestHandler(QObject):
                             voltage=site_settings.forward_bias_voltage,
                             delay=0.3,
                             step_size=10,
+                            execute_each_step=lambda: self.ramp_progress_bar(
+                            [site_settings.forward_bias_voltage] * len(self.instruments._module_dict.values())
+                            )
                         )
                     testName = "SCurveScan_2100"
                     hv_on_module = True
@@ -498,6 +510,9 @@ class TestHandler(QObject):
                         voltage=default_hv_voltage,
                         delay=0.3,
                         step_size=10,
+                        execute_each_step=lambda: self.ramp_progress_bar(
+                            [default_hv_voltage] * len(self.instruments._module_dict.values())
+                            )
                     )
 
         self.tempHistory = [0.0] * self.numChips
@@ -701,7 +716,7 @@ class TestHandler(QObject):
                 raise Exception(
                     f"Failed to copy root file to output directory. \
 Module disconnection detected because Ph2_ACF didn't \
-create {search_pattern}."
+create {search_pattern}"
                 )
 
             # Sort files by modification time (newest first)
