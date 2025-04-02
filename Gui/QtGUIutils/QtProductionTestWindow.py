b9cdb4a21a3af2f813680731543bbb0e6970ee93
mjoyce <joyce.279@osu.edu>
2025-04-02 16:15:44 -0400

    Fixed error where simplified gui freezes on "GO" Changed functionality to work with pyqtSignals. Also consolidated some signals in QtApplication.

--- Gui/QtGUIutils/QtProductionTestWindow.py
+++ Gui/QtGUIutils/QtProductionTestWindow.py
@@ -21,8 +21,6 @@ from Gui.QtGUIutils.QtMatplotlibUtils import ScanCanvas
 # from Gui.python.ResultTreeWidget import *
 # from Gui.python.TestValidator import *
 from Gui.python.IVCurveHandler import IVCurveHandler
-from Gui.python.SLDOScanHandler import SLDOScanHandler
-
 
 class QtProductionTestWindow(QWidget):
     resized = pyqtSignal()
--- Gui/QtGUIutils/QtStartWindow.py
+++ Gui/QtGUIutils/QtStartWindow.py
@@ -251,9 +251,6 @@ class SummaryBox(QWidget):
 
 
 class QtStartWindow(QWidget):
-    openRunWindowSignal = pyqtSignal()
-    errorMessageBoxSignal = pyqtSignal(str)
-
     def __init__(self, master, firmware):
         super(QtStartWindow, self).__init__()
         self.master = master
@@ -268,21 +265,6 @@ class QtStartWindow(QWidget):
         self.createApp()
         self.occupied()
         self.loading_counter = 0
-        self.openRunWindowSignal.connect(self.openRunWindowGUI)
-
-        self.errorMessageBoxSignal.connect(
-            lambda message: QMessageBox.information(
-                None,
-                "Error",
-                message,
-                QMessageBox.Ok,
-            )
-        )
-
-    def openRunWindowGUI(self):
-        self.master.RunNewTest = QtRunWindow(
-            self.master, self.info, self.firmwareDescription
-        )
 
     def setLoginUI(self):
         self.setGeometry(400, 400, 400, 400)
@@ -487,7 +469,7 @@ class QtStartWindow(QWidget):
 
         self.runFlag = True
         self.master.BeBoardWidget = self.BeBoardWidget
-        self.openRunWindowSignal.emit()
+        self.master.openRunWindowSignal.emit(self.info, self.firmwareDescription)
         self.close()
 
     def closeEvent(self, event):
