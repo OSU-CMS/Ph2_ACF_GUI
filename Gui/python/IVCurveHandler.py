2a88c2495c09535d1e9938eee861040d5f6db681
mjoyce <joyce.279@osu.edu>
2025-04-03 14:55:14 -0400

    personal commit (only the real ones know)

--- Gui/python/IVCurveHandler.py
+++ Gui/python/IVCurveHandler.py
@@ -44,7 +44,8 @@ class IVCurveThread(QThread):
         self.instruments.hv_off(
             execute_each_step=lambda: self.execute_each_step(starting_voltages)
         )
-        self.instruments.hv_on(voltage=0, delay=0.5, step_size=10, no_lock=True)
+        self.instruments.hv_on(voltage=0, delay=0.5, step_size=10, no_lock=True,
+                               execute_each_step=lambda: self.execute_each_step(starting_voltages))
 
     # Used to break out of hv_on correctly
     def breakTest(self):
