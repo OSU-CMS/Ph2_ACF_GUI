ab08ffe5f316eb57c09175bfd4cdb09aeba69852
Steven Speck <speck.57@osu.edu>
2025-04-09 14:25:32 -0400

    syntax fix

--- Gui/QtGUIutils/QtStartWindow.py
+++ Gui/QtGUIutils/QtStartWindow.py
@@ -419,7 +419,7 @@ class QtStartWindow(QWidget):
             self.close()
         else:
             self.NextButton.setText("&Next")
-            self.Nextbutton.setDisabled(False)
+            self.NextButton.setDisabled(False)
 
     def openRunWindow_starter(self):
         self.NextButton.setDisabled(True)
