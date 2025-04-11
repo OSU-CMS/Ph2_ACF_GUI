a1542a61371c9bc187aea7c7aef57a629c0d17b7
mjoyce <joyce.279@osu.edu>
2025-04-11 11:56:58 -0400

    minor typo

--- Gui/GUIutils/guiUtils.py
+++ Gui/GUIutils/guiUtils.py
@@ -410,6 +410,7 @@ def GenerateXMLConfig(BeBoard, testName, outputDir, **arg):
             RxPolarities = (
                 "1"
                 if "CROC" in moduleType
+                and "Quad" in moduleType
                 and "TFPX" in moduleType
                 else "0"
                 if "CROC" in moduleType
