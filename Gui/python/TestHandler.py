d18c7a1be16a340e047b2b185b65a7023b70b83b
mjoyce <joyce.279@osu.edu>
2025-04-18 16:38:53 -0400

    Fixed syntax and import errors

--- Gui/python/TestHandler.py
+++ Gui/python/TestHandler.py
@@ -481,7 +481,7 @@ class TestHandler(QObject):
                             for module in self.instruments._module_dict.values()
                         ]
                         self.instruments.hv_off(
-                            execute_each_step=lambda: self.execute_each_step(starting_voltages)
+                            execute_each_step=lambda: self.ramp_progress_bar(starting_voltages)
                         )
                         self.instruments.hv_on_module(
                             module=mod_dict[number],
