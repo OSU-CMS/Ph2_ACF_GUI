e8f9f95dbb48261b3703f005e2f0ddc7b014595f
Ryan De Los Santos <ryansantos1174@gmail.com>
2025-04-30 14:50:44 -0400

    Have tessie app display web page

--- Gui/QtGUIutils/TessieCoolingApp.py
+++ Gui/QtGUIutils/TessieCoolingApp.py
@@ -1,139 +1,28 @@
-from PyQt5 import QtWidgets
-from PyQt5.QtWidgets import QWidget
-from PyQt5.QtCore import Qt
+import webbrowser
+from PyQt5.QtWidgets import QWidget, QPushButton, QVBoxLayout
+from PyQt5.QtWebEngineWidgets import QWebEngineView
+from PyQt5.QtCore import QUrl
 
+from Gui.siteSettings import tessie_url, alternative_tessie_url
 
 class Tessie(QWidget):
-    def __init__(self, dimension):
+    def __init__(self, dimension=None):
         super(Tessie, self).__init__()
-        self.setupUi()
-        self.show()
+        layout = QVBoxLayout()
+        self.setLayout(layout)
 
-    def setupUi(self):
-        self.gridLayout = QtWidgets.QGridLayout(self)
+        open_button = QPushButton("Open Tessie Webpage in Browser")
+        open_button.clicked.connect(self.launch_tessie_webpage)
 
-        self.upperWidget = QWidget()
-        self.upperGridLayout = QtWidgets.QGridLayout(
-            self.upperWidget
-        )  # Set layout here
-        self.upperWidget.setLayout(self.upperGridLayout)
+        web_view = QWebEngineView()
+        web_view.load(QUrl(alternative_tessie_url))
+        web_view.setZoomFactor(0.8)
 
-        self.lowerWidget = QWidget()
-        self.lowerGridLayout = QtWidgets.QGridLayout(
-            self.lowerWidget
-        )  # Set layout here
-        self.lowerWidget.setLayout(self.lowerGridLayout)
+        layout.addWidget(open_button)
+        layout.addWidget(web_view)
 
-        self.gridLayout.addWidget(self.upperWidget, 0, 0)
-        self.upperGridLayout.addItem(QtWidgets.QSpacerItem(0, 10))
-        self.gridLayout.addWidget(self.lowerWidget, 1, 0)
+        if dimension:
+            self.setFixedSize(*dimension)
 
-        self.setLayout(self.gridLayout)  # Set layout for the main window
-
-        self.TessieLabel = QtWidgets.QLabel("TESSIE (2024/06/19-06)")
-        self.CANbus_errors_label = QtWidgets.QLabel("CANbus errors")
-        self.I2C_errors_label = QtWidgets.QLabel("I2C errors")
-        self.runtimeLabel = QtWidgets.QLabel("Runtime")
-        self.statusLabel = QtWidgets.QLabel("Status")
-
-        self.upperGridLayout.addWidget(self.TessieLabel, 0, 0, 1, 2)
-        self.upperGridLayout.addWidget(self.CANbus_errors_label, 1, 0, 1, 1)
-        self.upperGridLayout.addWidget(self.I2C_errors_label, 2, 0, 1, 1)
-        self.upperGridLayout.addWidget(self.runtimeLabel, 3, 0, 1, 1)
-        self.upperGridLayout.addWidget(self.statusLabel, 4, 0, 1, 1)
-
-        self.CANbus_errors_edit = QtWidgets.QLineEdit("0")
-        self.I2C_errors_edit = QtWidgets.QLineEdit("0")
-        self.runtimeEdit = QtWidgets.QLineEdit("10346")
-        self.statusEdit = QtWidgets.QLineEdit("no problem")
-
-        self.upperGridLayout.addWidget(self.CANbus_errors_edit, 1, 1, 1, 1)
-        self.upperGridLayout.addWidget(self.I2C_errors_edit, 2, 1, 1, 1)
-        self.upperGridLayout.addWidget(self.runtimeEdit, 3, 1, 1, 1)
-        self.upperGridLayout.addWidget(self.statusEdit, 4, 1, 1, 1)
-        self.upperGridLayout.setColumnMinimumWidth(1, 75)
-
-        self.airLabel = QtWidgets.QLabel("Air [° C]")
-        self.waterLabel = QtWidgets.QLabel("Water [° C]")
-        self.lid_status_label = QtWidgets.QLabel("Lid Status")
-
-        self.upperGridLayout.addWidget(self.airLabel, 1, 2, 1, 1)
-        self.upperGridLayout.addWidget(self.waterLabel, 2, 2, 1, 1)
-        self.upperGridLayout.addWidget(self.lid_status_label, 3, 2, 1, 1)
-
-        self.airEdit = QtWidgets.QLineEdit("23.25")
-        self.waterEdit = QtWidgets.QLineEdit("21.34")
-        self.lid_status_edit = QtWidgets.QLineEdit("locked")
-
-        self.airEdit.setStyleSheet(
-            "QLineEdit { background-color: green; color: black; }"
-        )
-        self.waterEdit.setStyleSheet(
-            "QLineEdit { background-color: green; color: black; }"
-        )
-        self.lid_status_edit.setStyleSheet(
-            "QLineEdit { background-color: green; color: black; }"
-        )
-
-        self.upperGridLayout.addWidget(self.airEdit, 1, 3, 1, 1)
-        self.upperGridLayout.addWidget(self.waterEdit, 2, 3, 1, 1)
-        self.upperGridLayout.addWidget(self.lid_status_edit, 3, 3, 1, 1)
-        self.upperGridLayout.setColumnMinimumWidth(3, 50)
-
-        self.ref_hum_label = QtWidgets.QLabel("Ref. Hum.")
-        self.dew_point_label = QtWidgets.QLabel("Dew Point")
-
-        self.upperGridLayout.addWidget(self.ref_hum_label, 1, 4, 1, 1)
-        self.upperGridLayout.addWidget(self.dew_point_label, 2, 4, 1, 1)
-
-        self.ref_hum_edit = QtWidgets.QLineEdit("51.92")
-        self.dew_point_edit = QtWidgets.QLineEdit("12.83")
-
-        self.ref_hum_edit.setStyleSheet(
-            "QLineEdit { background-color: yellow; color: black; }"
-        )
-        self.dew_point_edit.setStyleSheet(
-            "QLineEdit { background-color: green; color: black; }"
-        )
-
-        self.upperGridLayout.addWidget(self.ref_hum_edit, 1, 5, 1, 1)
-        self.upperGridLayout.addWidget(self.dew_point_edit, 2, 5, 1, 1)
-        self.upperGridLayout.setColumnMinimumWidth(5, 50)
-
-        self.stop_all_button = QtWidgets.QPushButton("STOP ALL")
-        self.stop_all_button.setStyleSheet(
-            "QPushButton {background-color: red; color: black}"
-        )
-        self.upperGridLayout.addWidget(self.stop_all_button, 4, 2, 1, 4)
-
-        self.flushButton = QtWidgets.QPushButton("Flush")
-        self.rinseButton = QtWidgets.QPushButton("Rinse")
-
-        self.lowerGridLayout.addWidget(self.flushButton, 0, 0)
-        self.lowerGridLayout.addWidget(self.rinseButton, 1, 0)
-
-        self.setTemperatures = [1] * 8
-        self.TEC_labels = [QtWidgets.QLabel(f"TEC {i}:") for i in range(1, 9)]
-        self.TEC_edits = [
-            QtWidgets.QLineEdit(f"{self.setTemperatures[i]}") for i in range(8)
-        ]
-
-        for i in range(4):
-            self.lowerGridLayout.addWidget(
-                self.TEC_labels[8 - i - 1], 0, 2 * i + 1, 1, 1, Qt.AlignRight
-            )
-            self.lowerGridLayout.addWidget(self.TEC_edits[8 - i - 1], 0, 2 * i + 2)
-            self.lowerGridLayout.addWidget(
-                self.TEC_labels[i], 1, 2 * i + 1, 1, 1, Qt.AlignRight
-            )
-            self.lowerGridLayout.addWidget(self.TEC_edits[i], 1, 2 * i + 2)
-
-        self.setLayout(self.gridLayout)
-
-
-if __name__ == "__main__":
-    import sys
-
-    app = QtWidgets.QApplication(sys.argv)
-    ui = Tessie(500)
-    sys.exit(app.exec_())
+    def launch_tessie_webpage(self):
+        webbrowser.open(tessie_url)
