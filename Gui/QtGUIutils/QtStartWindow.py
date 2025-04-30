5e225bf8f971084cc0ed40f37b51313ac8b7d4fb
mjoyce <joyce.279@osu.edu>
2025-04-30 10:58:32 -0400

    Remove QtProductionTestWindow

--- Gui/QtGUIutils/QtStartWindow.py
+++ Gui/QtGUIutils/QtStartWindow.py
@@ -46,7 +46,6 @@ logger = logging.getLogger(__name__)
 # from Gui.python.Firmware import *
 # from Gui.GUIutils.DBConnection import *
 
-# from Gui.QtGUIutils.QtProductionTestWindow import *
 
 
 class SummaryBox(QWidget):
