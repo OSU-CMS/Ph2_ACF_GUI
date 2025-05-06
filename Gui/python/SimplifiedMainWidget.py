233a2cda83018034f2f003de5f44b5f041f0dbec
mjoyce <joyce.279@osu.edu>
2025-05-06 13:45:27 -0400

    implemented docker continuous integration

--- Gui/python/SimplifiedMainWidget.py
+++ Gui/python/SimplifiedMainWidget.py
@@ -36,7 +36,6 @@ class SimplifiedMainWidget(QWidget):
     def __init__(self, master, dimension):
         logger.debug("SimplifiedMainWidget.__init__()")
         super().__init__()
-        print("docking test 6")
         self.master = master
         self.dimension = dimension
 
