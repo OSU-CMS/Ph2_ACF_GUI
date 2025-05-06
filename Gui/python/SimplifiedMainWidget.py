7f0cfa7606105916a667180140c08b80139c29ae
mjoyce <joyce.279@osu.edu>
2025-05-06 13:01:45 -0400

    docking test 5

--- Gui/python/SimplifiedMainWidget.py
+++ Gui/python/SimplifiedMainWidget.py
@@ -36,7 +36,7 @@ class SimplifiedMainWidget(QWidget):
     def __init__(self, master, dimension):
         logger.debug("SimplifiedMainWidget.__init__()")
         super().__init__()
-        print("docking test 4")
+        print("docking test 5")
         self.master = master
         self.dimension = dimension
 
