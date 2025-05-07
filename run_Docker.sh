cccf831e4de8f3d76e9a702c9d317df0fe4a1f01
Ryan De Los Santos <ryansantos1174@gmail.com>
2025-05-07 15:14:16 -0400

    Adding in automatic docker image creation
    * Added continuous integration for Docker
    
    * Automation with Docker for CI/CD Workflows

--- run_Docker.sh
+++ run_Docker.sh
@@ -56,7 +56,7 @@ then
 		-v /tmp/.X11-unix:/tmp/.X11-unix --net host --entrypoint /bin/bash coldbox_testing
 else
     echo "running as user"
-	IMAGE_NAME="majoyce2/ph2_acf_gui_user:latest"
+	IMAGE_NAME="osupixels/ph2_acf_gui_user:latest"
 
         # Check if skopleo is installed, if not prompt user to install
         if ! which skopeo > /dev/null 2>&1; then
@@ -98,7 +98,7 @@ To install on Alma Linux please run:\e[0m
 		-v ${PWD}/Gui/QtGUIutils/PeltierCoolingApp.py:/home/cmsTkUser/Ph2_ACF_GUI/Gui/QtGUIutils/PeltierCoolingApp.py\
         -v ${PWD}/Gui/python/Peltier.py:/home/cmsTkUser/Ph2_ACF_GUI/Gui/python/Peltier.py\
 		-w $PWD  -e DISPLAY=$DISPLAY\
-		-v /tmp/.X11-unix:/tmp/.X11-unix --net host coldbox_testing #local/testimagejuly30user
+		-v /tmp/.X11-unix:/tmp/.X11-unix --net host majoyce2/ph2_acf_gui_user:latest #local/testimagejuly30user
 		#Before, the docker run command had the options -v $XSOCK:$XSOCK -v $XAUTH:$XAUTH -e XAUTHORITY=$XAUTH. We were having trouble
 		#running the GUI through SSH connections, so we removed those options and added --volume="$HOME/.Xauthority:/root/.Xauthority:rw"
 		#which seemed to fix the issue of running the GUI from SSH connections. At the time of this commit, we have no idea why this fixed it
