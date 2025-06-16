2c1c2e310598a6b0c1e5c1804eb7cd9c28ef3b20
mjoyce <joyce.279@osu.edu>
2025-06-16 11:08:15 -0400

    updating for felisv1.1.1

--- Gui/python/TestHandler.py
+++ Gui/python/TestHandler.py
@@ -1700,7 +1700,7 @@ created by Ph2_ACF is empty."
                     self.master.password,
                     type_sequence=self.info,
                     version_ph2acf=os.environ.get("PH2ACF_VERSION"),
-                    #version_testStationSoftware=os.environ.get("PH2_ACF_GUI_VERSION"),
+                    version_testStationSoftware=os.environ.get("PH2_ACF_GUI_VERSION"),
                 )
                 if not status:
                     raise ConnectionError(message)
--- check_configuration_files.sh
+++ check_configuration_files.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-REQUIRED_VERSION=1 # Will need to change this by hand
+REQUIRED_VERSION=2 # Will need to change this by hand
 
 # Loop over all files passed as arguments
 for FILE in "$@"; do
