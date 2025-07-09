2c4d1feab2b4d466f9eeb2da911d3c9b694230df
a2ronmeade <meade.246@buckeyemail.osu.edu>
2025-07-09 10:59:11 -0400

    fixed vref attribute

--- Gui/GUIutils/guiUtils.py
+++ Gui/GUIutils/guiUtils.py
@@ -489,7 +489,7 @@ def GenerateXMLConfig(BeBoard, testName, outputDir, txt_files:dict, **arg):
                 FEChip.VDDAtrim = chip.getVDDA()
                 FEChip.VDDDtrim = chip.getVDDD()
                 FEChip.EfuseID = chip.getEfuseID()
-                FEChip.VREF_ADC = chip.getVREF()
+                FEChip.VREF = chip.getVREF()
 
                 HyBridModule0.AddFE(FEChip)
             HyBridModule0.ConfigureGlobal(globalSettings_Dict[testName])
