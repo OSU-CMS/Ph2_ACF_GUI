833381f8348634ae5649b4db7014d00c75cbdd34
Ryan De Los Santos <ryansantos1174@gmail.com>
2025-07-22 16:19:13 -0400

    Fix import

--- Gui/python/SimplifiedMainWidget.py
+++ Gui/python/SimplifiedMainWidget.py
@@ -4,7 +4,7 @@ from serial import SerialException
 from typing import Optional
 
 from Gui.QtGUIutils.QtStartWindow import SummaryBox
-from PyQt5.QtCore import Qt, QSize, pyqtSignal, QObject, QThread
+from PyQt5.QtCore import Qt, QSize, pyqtSignal, QObject, QThread, QTimer
 from PyQt5.QtGui import QPixmap, QImage, QIcon
 from PyQt5.QtWidgets import (
     QGridLayout,
@@ -15,7 +15,6 @@ from PyQt5.QtWidgets import (
     QHBoxLayout,
     QVBoxLayout,
     QWidget,
-    QTimer,
     QMessageBox,
 )
 
