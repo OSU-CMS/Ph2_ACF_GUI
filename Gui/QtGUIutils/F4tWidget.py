ee0ffe7b2db860631e6afa0265c9e0cfc6e68f57
Ryan De Los Santos <ryansantos1174@gmail.com>
2025-08-19 16:23:07 -0400

    Change Module input to QDialog

--- Gui/QtGUIutils/F4tWidget.py
+++ Gui/QtGUIutils/F4tWidget.py
@@ -1,12 +1,12 @@
 from PyQt5 import QtWidgets
-from PyQt5.QtCore import pyqtSignal
 
-class F4TModuleInputWidget(QtWidgets.QWidget):
-    input_modules_signal = pyqtSignal(list)
+class F4TModuleInputWidget(QtWidgets.QDialog):
     def __init__(self, initial_inputs=5, parent=None):
         super().__init__(parent)
 
-        # Main layout for the widget
+        self.setWindowTitle("Enter Module IDs")
+        self.resize(400, 300)
+
         main_layout = QtWidgets.QVBoxLayout(self)
 
         # Scroll area setup
@@ -17,13 +17,8 @@ class F4TModuleInputWidget(QtWidgets.QWidget):
         # Inner widget that holds the input fields
         self.inner_widget = QtWidgets.QWidget()
         self.inner_layout = QtWidgets.QVBoxLayout(self.inner_widget)
-        self.inner_layout.setContentsMargins(5, 5, 5, 5)
-        self.inner_layout.setSpacing(1)
 
-        # Add button
-        self.add_button = QtWidgets.QPushButton("Add Input Field")
-        self.add_button.clicked.connect(self.add_input_field)
-        main_layout.addWidget(self.add_button)
+        self.scroll_area.setWidget(self.inner_widget)
 
         # Store references to input fields
         self.input_fields = []
@@ -32,7 +27,18 @@ class F4TModuleInputWidget(QtWidgets.QWidget):
         for _ in range(initial_inputs):
             self.add_input_field()
 
-        self.scroll_area.setWidget(self.inner_widget)
+        # Add Input button
+        self.add_button = QtWidgets.QPushButton("Add Input Field")
+        self.add_button.clicked.connect(self.add_input_field)
+        main_layout.addWidget(self.add_button)
+
+        # OK/Cancel buttons
+        button_box = QtWidgets.QDialogButtonBox(
+            QtWidgets.QDialogButtonBox.Ok | QtWidgets.QDialogButtonBox.Cancel
+        )
+        button_box.accepted.connect(self.accept)
+        button_box.rejected.connect(self.reject)
+        main_layout.addWidget(button_box)
 
     def add_input_field(self):
         """Adds a new input field."""
@@ -42,5 +48,5 @@ class F4TModuleInputWidget(QtWidgets.QWidget):
         self.input_fields.append(line_edit)
 
     def get_all_inputs(self):
-        """Return a list of all entered values."""
-        self.input_modules_signal.emit([field.text() for field in self.input_fields])
+        """Return non-empty values from input fields."""
+        return [f.text().strip() for f in self.input_fields if f.text().strip()]
