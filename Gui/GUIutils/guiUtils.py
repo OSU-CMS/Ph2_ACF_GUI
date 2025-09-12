ce7fa17b712c9babb1244b1dc160603e56ffc420
pixels <pixels@localhost.localdomain>
2025-09-12 14:41:35 -0400

    Fixed line 471: chip settings

--- Gui/GUIutils/guiUtils.py
+++ Gui/GUIutils/guiUtils.py
@@ -469,6 +469,7 @@ def GenerateXMLConfig(BeBoard, testName, outputDir, txt_files:dict, **arg):
 
                 chip_settings = FESettings_Dict[testName][registerKey].copy()
                 chip_settings['VREF_ADC'] = chip.getVREF()
+                chip_settings['INJ_CAP'] = chip.getCINJ()
                 FEChip.ConfigureFE(chip_settings)
             
                 if testName in FELaneConfig_Dict:
