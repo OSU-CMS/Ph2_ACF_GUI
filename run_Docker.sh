fb257ceda4b0ad4295ccf0928eac933ea2055cce
pixels <pixels@localhost.localdomain>
2026-01-08 15:25:40 -0500

    updating image name for v4.0.0 release

--- run_Docker.sh
+++ run_Docker.sh
@@ -7,7 +7,7 @@ bash check_configuration_files.sh run_Docker.sh Gui/siteConfig.py
 SOCK=/tmp/.X11-unix; XAUTH=/tmp/.docker.xauth; xauth nlist $DISPLAY | sed -e 's/^..../ffff/' | xauth -f $XAUTH nmerge -; chmod 777 $XAUTH;
 
 ######### Specify the docker image to use #################
-IMAGE_NAME="osupixels/ph2_acf_gui_dev:v4.0.0-pre.1"
+IMAGE_NAME="osupixels/ph2_acf_gui_dev:v4.0.0"
 #IMAGE_NAME="majoyce2/ph2_acf_gui_purdue:latest"
 #IMAGE_NAME="majoyce2/ph2_acf_gui_user:latest"
 #IMAGE_NAME="local/testimagemay29user"
