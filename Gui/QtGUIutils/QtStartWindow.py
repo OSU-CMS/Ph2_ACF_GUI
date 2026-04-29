6c28fae3449aa1d95e0fc07e791fa60ebd6902bb
Matthew Joyce <joyce.279@osu.edu>
2026-04-29 09:38:30 -0400

    Making firmware image load to multiple fc7s for multimodule testing

--- Gui/QtGUIutils/QtStartWindow.py
+++ Gui/QtGUIutils/QtStartWindow.py
@@ -104,7 +104,7 @@ class SummaryBox(QWidget):
             # updating uri value in template xml file with correct fc7 ip address, as specified in siteSettings.py
             # fc7_ip = site_settings.FC7List[pfirmwareName] #Commented because I don't think we need it.  Remove line after test.
             print("The fc7 ip is: {0}".format(fc7_ip))
-            uricmd = "sed -i -e 's/fc7-1/{0}/g' {1}/Gui/CMSIT_{2}.xml".format(
+            uricmd = "sed -i -e 's/target=[^:]*/target={0}/' {1}/Gui/CMSIT_{2}.xml".format(
                 fc7_ip, os.environ.get("GUI_dir"), boardtype
             )
             subprocess.call([uricmd], shell=True)
